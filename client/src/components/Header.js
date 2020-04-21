058706a1a12e61e92f02404e2640d10be26b73be
Ionut Chioran <ionut.chioran@softvision.ro>
2020-04-21 20:58:32 +0300

    clean up

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -21,4 +21,5 @@ export const NavigationHeader = () => (
                 </LiItem>
             </LinkList>
     </HeaderContainer>
+
 );
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
@@ -8,19 +7,6 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/api/ibm', require('./routes/ibm'));
-
-
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-    console.log('We are prod');
-    // Set static folder
-    app.use(express.static("client/build"));
-
-    // index.html for all page routes
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-}
 
 const port = process.env.PORT || 5000;
 
